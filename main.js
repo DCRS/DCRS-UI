7b570b0e26b5bdf357e4c77944732e15a0882525
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-08-02 03:14:26 -0300

    Test to re-use Electron

--- main.js
+++ main.js
@@ -1,107 +1,24 @@
-const electron = require('electron')
-// Module to control application life.
+// TODO: you will implement this!
+const electron = require('electron');
 const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;app.commandLine.appendSwitch('--enable-experimental-web-platform-features');
+const fs = require('fs');
+const path = require('path');
+const jQuery = require('jquery');
+const username = require('username');
+var sudo = require('sudo-prompt');
+var DecompressZip = require('decompress-zip');
 
-/*const path = require('path')
-const url = require('url')*/
+let mainWindow = null
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+app.on('ready', () => {
+  console.log('The application is ready.')
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    "type": "",
-    "height": 600,
-    "resizable": false,
-    "exeIcon": "icon.png",
-    "transparent": false,
-    "macIcon": "icon.png",
-    "fullscreen": false,
-    "x": 100,
-    "maxHeight": "",
-    "title": "DCRS",
-    "minHeight": "",
-    "skipTaskbar": false,
-    "acceptFirstMouse": false,
-    "frame": true,
-    "y": 100,
-    "darkTheme": false,
-    "kiosk": true,
-    "width": 800,
-    "backgroundColor": "#000000",
-    "alwaysOnTop": true,
-    "show": true,
-    "icon": "favicon.png",
-    "webPreferences": {
-      "preload": "",
-      "textAreasAreResizable": true,
-      "javascript": true,
-      "webSecurity": true,
-      "experimentalCanvasFeatures": false,
-      "directWrite": true,
-      "zoomFactor": 1.0,
-      "allowRunningInsecureContent": false,
-      "blinkFeatures": "",
-      "allowDisplayingInsecureContent": false,
-      "partition": "",
-      "plugins": true,
-      "webgl": true,
-      "nodeIntegration": true,
-      "webaudio": true,
-      "experimentalFeatures": false,
-      "images": true
-    },
-    "titleBarStyle": "default",
-    "center": true,
-    "minWidth": "",
-    "maxWidth": "",
-    "autoHideMenuBar": false,
-    "disableAutoHideCursor": false,
-    "useContentSize": false,
-    "enableLargerThanScreen": false
-  })
-  // and load the index.html of the app.
-  mainWindow.loadURL('http://127.0.0.1:43110/1DCRSfb95muP5N78v1fQ8DN8CxbjEfXewT/ui/')
+  mainWindow = new BrowserWindow()
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  mainWindow.loadURL('file://' + path.join(__dirname, 'index.html'))
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
+  mainWindow.on('closed', function() {
     mainWindow = null
   })
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
 })
-
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-
-require("child_process").spawn("/snap/dcrs/current/bin/zeronet",[],{stdio:"inherit"})
